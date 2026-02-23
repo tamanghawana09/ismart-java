3931cb36c407e3b4361e15198a68bc0583cb9ff2
tamanghawana09 <hawanatamang@gmail.com>
2026-02-23 15:06:33 +0545

    code: Security authentications, request matchers

--- crud-application/src/main/java/com/example/crudapplication/AdminSecurityConfiguration.java
+++ crud-application/src/main/java/com/example/crudapplication/AdminSecurityConfiguration.java
@@ -1,4 +1,40 @@
 package com.example.crudapplication;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@Order(2)
 public class AdminSecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain adminSecurityFilterChain(HttpSecurity http, AdminService adminService) throws Exception{
+        http
+                .securityMatcher("/admin/**")
+                .userDetailsService(adminService)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/admin/login","/admin/register","/admin/save").permitAll()
+                        .requestMatchers("/admin/dashboard/**").hasAuthority("ADMIN")
+                        .anyRequest().authenticated()
+                )
+                .formLogin(form-> form
+                        .loginPage("/admin/login")
+                        .loginProcessingUrl("/admin/login")
+                        .usernameParameter("email")
+                        .passwordParameter("password")
+                        .successHandler((request, response, authentication) -> {
+                            response.sendRedirect("/admin/dashboard");
+                        })
+                        .permitAll()
+                )
+                .logout(logout -> logout
+                        .logoutUrl("/admin/logout")
+                        .logoutSuccessUrl("/admin/login")
+                        .permitAll()
+                );
+        return http.build();
+    }
 }
