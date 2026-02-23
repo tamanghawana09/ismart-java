c8dbb6bc205567b11ca0a8eb83d07918151e78a2
tamanghawana09 <hawanatamang@gmail.com>
2026-02-23 15:10:37 +0545

    code: Add user service

--- crud-application/src/main/java/com/example/crudapplication/Users.java
+++ crud-application/src/main/java/com/example/crudapplication/Users.java
@@ -12,6 +12,9 @@ public class Users {
     private String username;
     private String password;
     private String email;
+    private String role;
+    private Long studentAdded;
+
 
     @Version
     private Integer version;
@@ -19,10 +22,12 @@ public class Users {
     public Users(){}
 
     //Constructor
-    public Users(String username,String password,String email){
+    public Users(String username,String password,String email,String role){
         this.username = username;
         this.password = password;
         this.email = email;
+        this.role = role;
+
     }
 
     //getters & setters
@@ -47,4 +52,10 @@ public class Users {
     }
     public String getEmail(){ return email; }
     public void setEmail(String email){this.email = email; }
+    public String getRole(){ return role;}
+    public void setRole(String role){this.role = role;}
+
+
+//    public Long getStudentAdded(){return studentAdded;}
+//    public void setStudentAdded(Long studentAdded){this.studentAdded = studentAdded;}
 }
